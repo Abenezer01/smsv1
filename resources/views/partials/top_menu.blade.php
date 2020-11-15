d0e7eb0fef3d2cf2641cccda3802f3c89650f1df
Abenezer01 <abenezerseyoum7@gmail.com>
2020-11-15 14:32:04 +0300

    First Commit

--- resources/views/partials/top_menu.blade.php
+++ resources/views/partials/top_menu.blade.php
@@ -26,26 +26,20 @@
                     <i class="icon-paragraph-justify3"></i>
                 </a>
             </li>
-
-
         </ul>
-
 			<span class="navbar-text ml-md-3 mr-md-auto"></span>
-
         <ul class="navbar-nav">
-
             <li class="nav-item dropdown dropdown-user">
                 <a href="#" class="navbar-nav-link dropdown-toggle" data-toggle="dropdown">
                     <img style="width: 38px; height:38px;" src="{{ Auth::user()->photo }}" class="rounded-circle" alt="photo">
                     <span>{{ Auth::user()->name }}</span>
                 </a>
-
                 <div class="dropdown-menu dropdown-menu-right">
                     <a href="{{ Qs::userIsStudent() ? route('students.show', Qs::hash(Qs::findStudentRecord(Auth::user()->id)->id)) : route('users.show', Qs::hash(Auth::user()->id)) }}" class="dropdown-item"><i class="icon-user-plus"></i> My profile</a>
                     <div class="dropdown-divider"></div>
                     <a href="{{ route('my_account') }}" class="dropdown-item"><i class="icon-cog5"></i> Account settings</a>
                     <a href="{{ route('logout') }}" onclick="event.preventDefault();
-          document.getElementById('logout-form').submit();" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
+                 document.getElementById('logout-form').submit();" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
